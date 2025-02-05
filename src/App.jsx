826dcbf82a3614efb53c0731abb78ae41a7a7079
kabi-60 <kabilashi2005@gmail.com>
2025-02-05 07:17:46 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
+import Skill from './Components/Skill'
+import About from './Components/About'
 
 const App = () => {
   return (
     <div className='container mx-auto'>
       <Navbar/>
       <Home/>
+      <About/>
+      <Skill/>
     </div>
   )
 }
---
+++ src/Components/Skill.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SkillCard from './SkillCard'
+
+const Skill = () => {
+  return (
+    <div className='mt-20'>
+        <div className="flex justify-between">
+      <SkillCard/>
+        <h1 className="font-semibold text-6xl text-[#171717] ">
+         About Me
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Skill
